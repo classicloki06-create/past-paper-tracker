fc280cad7c1476cf30d87e9ecb2d69d7659f59ba
classicloki06-create <classicloki06@gmail.com>
2026-08-31 19:25:16 +0530

    Refactor firebase config to export configuration

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,17 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyDR8jO4X-DFvmyLPXYddiZ-62B7iXmWiaA",
   authDomain: "past-paper-tracker-2d695.firebaseapp.com",
   projectId: "past-paper-tracker-2d695",
   storageBucket: "past-paper-tracker-2d695.firebasestorage.app",
   messagingSenderId: "103628874739",
   appId: "1:103628874739:web:59f85dc4f95087d37fa886"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+}
